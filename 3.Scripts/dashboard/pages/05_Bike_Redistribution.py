35cbf3a5ef8e069e01809cfb0ad6776570a5bbe2
walkwithlack <magi.apostoli@gmail.com>
2025-10-05 00:56:39 +0200

    Fix missing Streamlit import in Bike_Redistribution.py

--- 3.Scripts/dashboard/pages/05_Bike_Redistribution.py
+++ 3.Scripts/dashboard/pages/05_Bike_Redistribution.py
@@ -1,6 +1,7 @@
 # Data files required for this dashboard are hosted on Google Drive:
-# https://drive.google.com/drive/folders/18dn7QjYPa3z1ZIkUEGr9zHzWnwQxSUMzimport streamlit as st
+# https://drive.google.com/drive/folders/18dn7QjYPa3z1ZIkUEGr9zHzWnwQxSUMz
 
+import streamlit as st
 import pandas as pd
 import numpy as np
 import folium
